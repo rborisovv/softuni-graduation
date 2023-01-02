477771fa68032b9cec64da48577001aa004949f3
rborisovv <radii2000@abv.bg>
2023-01-02 22:56:36 +0200

    Added logout functionality in the admin header component

--- Angular/front-end/src/app/administration/admin-header/admin-header.component.ts
+++ Angular/front-end/src/app/administration/admin-header/admin-header.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {faAngleDown, faCartShopping, faSignOut} from '@fortawesome/free-solid-svg-icons';
+import {CookieService} from "ngx-cookie-service";
+import {UserService} from "../../service/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-admin-header',
@@ -14,9 +17,22 @@ export class AdminHeaderComponent implements OnInit {
 
   faSignOut = faSignOut;
 
-  constructor() { }
+  constructor(private cookieService: CookieService, private userService: UserService,
+              private router: Router) {
+  }
 
   ngOnInit(): void {
   }
 
+
+  logout(): void {
+    this.userService.logoutUser().subscribe({
+      next: () => {
+        this.cookieService.delete('JWT-TOKEN', '/', 'localhost');
+      },
+      complete: () => {
+        this.router.navigateByUrl('/auth/login');
+      }
+    });
+  }
 }
