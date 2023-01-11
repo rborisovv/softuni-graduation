8f8fc332e653b46d9dcb493f8324dee3ac24c6c7
rborisovv <radii2000@abv.bg>
2023-01-11 17:41:23 +0200

    Added functionality for deleting a media

--- Java/softuni-graduation/src/main/java/bg/rborisov/softunigraduation/common/Messages.java
+++ Java/softuni-graduation/src/main/java/bg/rborisov/softunigraduation/common/Messages.java
@@ -3,8 +3,8 @@ package bg.rborisov.softunigraduation.common;
 public class Messages {
     public static final String CATEGORY_CREATED = "The category \"%s\" was created successfully!";
     public static final String CATEGORY_UPDATED = "The category \"%s\" was updated successfully!";
-    public static final String IMAGES_CONSTRAINT_MESSAGE = "Only image files are supported (PNG & JPG)";
-    public static final String CATEGORY_DELETED_SUCCESSFULLY = "The category \"%s\" was successfully deleted!";
-
+    public static final String IMAGES_CONSTRAINT_MESSAGE = "Only image files are supported (PNG & JPG)!";
+    public static final String CATEGORY_DELETED_SUCCESSFULLY = "The category \"%s\" was deleted successfully!";
     public static final String MEDIA_CREATED_SUCCESSFULLY = "The media was successfully created!";
+    public static final String MEDIA_DELETED_SUCCESSFULLY = "The media \"%s\" was deleted successfully!";
 }
