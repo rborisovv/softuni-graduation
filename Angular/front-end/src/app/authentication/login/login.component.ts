1edeaddb16e2540ec7e6836dbde62a6faad63f2c
Radoslav <radii2000@abv.bg>
2023-03-22 20:37:33 +0200

    Visual fixes

--- Angular/front-end/src/app/authentication/login/login.component.ts
+++ Angular/front-end/src/app/authentication/login/login.component.ts
@@ -11,6 +11,7 @@ import { loginAction } from "../../store/action/auth.action";
 import { NotifierService } from "angular-notifier";
 import { NotificationType } from "../../enumeration/notification-enum";
 import { resetPassword } from "../../store/action/user.action";
+import { take } from "rxjs";
 
 @Component({
   selector: 'app-login',
@@ -61,8 +62,8 @@ export class LoginComponent implements OnInit {
     })
     //TODO: Remove the event listeners manually
 
-    // this.userService.obtainCsrf()
-    //   .pipe(take(1)).subscribe();
+    this.userService.obtainCsrf()
+      .pipe(take(1)).subscribe();
   }
 
   public onLogin() {
