d27094049dc575731135fe1c092a0b319262690e
Radoslav <radii2000@abv.bg>
2023-03-26 20:02:00 +0300

    Added additional Administration components Added Users component implementation

---
+++ Angular/front-end/src/app/administration/user/user.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from "../../service/user.service";
+import { Observable } from "rxjs";
+import { User } from "../../interface/user";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent implements OnInit {
+  users$: Observable<User[]>;
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.users$ = this.userService.loadAllUsers();
+  }
+
+}
