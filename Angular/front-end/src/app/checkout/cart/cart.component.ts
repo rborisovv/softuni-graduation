60260afa412f3ca3eaaaf658d6f6099398169ab8
Radoslav <radii2000@abv.bg>
2023-03-31 22:18:27 +0300

    Voucher can now be used in the cart and the value updates, when product is removed

--- Angular/front-end/src/app/checkout/cart/cart.component.ts
+++ Angular/front-end/src/app/checkout/cart/cart.component.ts
@@ -1,6 +1,6 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { UserService } from "../../service/user.service";
-import { Observable, take } from "rxjs";
+import { Observable, take, tap } from "rxjs";
 import { Product } from "../../model/product";
 import { Location } from "@angular/common";
 import { Store } from "@ngrx/store";
@@ -9,38 +9,31 @@ import { selectBasketProductsState } from "../../store/selector/user.selector";
 import { Router } from "@angular/router";
 import { BasketService } from "../../service/basket.service";
 import { Voucher } from "../../model/voucher";
+import { NotifierService } from "angular-notifier";
+import { NotificationType } from "../../enumeration/notification-enum";
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent implements OnInit {
+export class CartComponent implements OnInit, AfterViewInit {
   renewedBasketProducts$: Observable<Product[]>;
   voucher$: Observable<Voucher | null>;
 
   totalAfterDiscount: Observable<number>;
 
   @ViewChild('totalPrice') totalPrice: ElementRef | undefined;
+  @ViewChild('voucherInput') voucherInput: ElementRef;
+  @ViewChild('voucherLabel') voucherLabel: ElementRef;
 
   constructor(private userService: UserService, protected location: Location,
-              private store: Store, public router: Router, private basketService: BasketService) {
+              private store: Store, public router: Router, private basketService: BasketService,
+              private renderer: Renderer2, private notifier: NotifierService) {
   }
 
   ngOnInit(): void {
     this.renewedBasketProducts$ = this.userService.loadBasketProducts();
-    // this.voucher$ = this.basketService.fetchVoucherIfPresent().pipe(take(1), tap((voucher) => {
-    //   if (voucher) {
-    //     this.totalAfterVoucherDiscount = this.cartBalance.transform(this.renewedBasketProducts$)
-    //       .pipe(take(1), switchMap(total => {
-    //         if (voucher.type === 'RELATIVE') {
-    //           return of(total - (voucher.discount / 100) * total);
-    //         }
-    //
-    //         return of(total - voucher.discount);
-    //       }));
-    //   }
-    // }), switchMap(voucher => of(voucher)));
   }
 
   removeProductFromBasket(identifier: string) {
@@ -57,5 +50,16 @@ export class CartComponent implements OnInit {
   addVoucher(labelElement: HTMLInputElement) {
     const voucher: string = labelElement.value;
     this.voucher$ = this.basketService.addVoucherToBasket(voucher).pipe(take(1));
+    this.notifier.notify(NotificationType.SUCCESS, `The voucher ${voucher} has been used successfully!`);
+    this.renderer.setStyle(this.voucherLabel.nativeElement, 'bottom', '100%');
+  }
+
+  ngAfterViewInit(): void {
+    this.voucher$ = this.basketService.fetchVoucherIfPresent().pipe(tap((v) => {
+      if (v) {
+        this.voucherInput.nativeElement.value = v.name;
+        this.renderer.setStyle(this.voucherLabel.nativeElement, 'bottom', '100%');
+      }
+    }));
   }
 }
