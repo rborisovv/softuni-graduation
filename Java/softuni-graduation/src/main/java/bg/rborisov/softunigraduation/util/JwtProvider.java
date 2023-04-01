691c50738722124be5e8085c23e3cfffed78c4c1
Radoslav <radii2000@abv.bg>
2023-04-01 19:24:39 +0300

    Added initial implementation of RsaKeysIntegrity verification

--- Java/softuni-graduation/src/main/java/bg/rborisov/softunigraduation/util/JwtProvider.java
+++ Java/softuni-graduation/src/main/java/bg/rborisov/softunigraduation/util/JwtProvider.java
@@ -1,6 +1,5 @@
 package bg.rborisov.softunigraduation.util;
 
-import bg.rborisov.softunigraduation.exception.RsaKeyIntegrityViolationException;
 import bg.rborisov.softunigraduation.exception.UserNotFoundException;
 import bg.rborisov.softunigraduation.service.UserService;
 import com.auth0.jwt.JWT;
@@ -18,8 +17,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -33,18 +30,14 @@ public final class JwtProvider {
     private final UserDetailsService userDetailsService;
     private final UserService userService;
     private final RSAKeyProvider rsaKeyProvider;
-    private final RsaKeyIntegrityVerifier rsaKeyIntegrityVerifier;
 
-    public JwtProvider(UserDetailsService userDetailsService, @Lazy UserService userService, RSAKeyProvider rsaKeyProvider, RsaKeyIntegrityVerifier rsaKeyIntegrityVerifier) {
+    public JwtProvider(UserDetailsService userDetailsService, @Lazy UserService userService, RSAKeyProvider rsaKeyProvider) {
         this.userDetailsService = userDetailsService;
         this.userService = userService;
         this.rsaKeyProvider = rsaKeyProvider;
-        this.rsaKeyIntegrityVerifier = rsaKeyIntegrityVerifier;
     }
 
-    public String generateToken() throws RsaKeyIntegrityViolationException, IOException, NoSuchAlgorithmException {
-        this.rsaKeyIntegrityVerifier.verifyRsaKeysIntegrity();
-
+    public String generateToken() {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         String[] claims = getClaimsFromUser(username);
         try {
