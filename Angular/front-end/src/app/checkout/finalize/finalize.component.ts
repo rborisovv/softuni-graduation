6174c0e6398eaf2bb8145687c79b45e40f090cca
Radoslav <radii2000@abv.bg>
2023-04-01 14:34:35 +0300

    Voucher functionality final commit for adding to basket and through checkout flow

--- Angular/front-end/src/app/checkout/finalize/finalize.component.ts
+++ Angular/front-end/src/app/checkout/finalize/finalize.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, take } from "rxjs";
+import { map, Observable, of, take } from "rxjs";
 import { Product } from "../../model/product";
 import { Router } from "@angular/router";
 import { Store } from "@ngrx/store";
@@ -7,6 +7,9 @@ import { Location } from "@angular/common";
 import { UserService } from "../../service/user.service";
 import { Checkout } from "../../model/checkout";
 import { createOrder } from "../../store/action/user.action";
+import { Voucher } from "../../model/voucher";
+import { selectDiscountedTotalState } from "../../store/selector/basket.selector";
+import { BasketService } from "../../service/basket.service";
 
 @Component({
   selector: 'app-finalize',
@@ -15,9 +18,11 @@ import { createOrder } from "../../store/action/user.action";
 })
 export class FinalizeComponent implements OnInit {
   checkoutData: Checkout;
+  discountedBasketTotal$: Observable<string> = of(undefined);
+  voucher$: Observable<Voucher> = of(undefined);
 
-  constructor(private readonly store: Store, protected readonly router: Router, protected readonly location: Location,
-              private readonly userService: UserService) {
+  constructor(private store: Store, protected router: Router, protected location: Location,
+              private userService: UserService, private basketService: BasketService) {
   }
 
   renewedBasketProducts$: Observable<Product[]>;
@@ -26,6 +31,12 @@ export class FinalizeComponent implements OnInit {
     this.renewedBasketProducts$ = this.userService.loadBasketProducts();
     this.userService.fetchCheckoutDataIfPresent().pipe(take(1))
       .subscribe(data => this.checkoutData = data);
+
+    this.voucher$ = this.basketService.fetchVoucherIfPresent();
+
+    this.discountedBasketTotal$ = this.store.select(selectDiscountedTotalState).pipe(
+      map(state => state ? (state.total <= 0 ? 'Free' : (state.total.toFixed(2) + ' lv.')) : undefined)
+    );
   }
 
   createOrder() {
