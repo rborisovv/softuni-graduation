4f2df321381abf6b8e169c59846a6bf5c14636f8
rborisovv <radii2000@abv.bg>
2023-04-07 16:31:11 +0300

    Added additional startup job for product restocking

--- Java/softuni-graduation/src/main/java/bg/rborisov/softunigraduation/task/ApplicationStartupJobs.java
+++ Java/softuni-graduation/src/main/java/bg/rborisov/softunigraduation/task/ApplicationStartupJobs.java
@@ -12,21 +12,30 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 @Component
-public class ProductBestBeforeUpdateJob {
+public class ApplicationStartupJobs {
     private final ProductRepository productRepository;
 
-    public ProductBestBeforeUpdateJob(ProductRepository productRepository) {
+    public ApplicationStartupJobs(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     @PostConstruct
     @Transactional
-    public void update() {
+    public void productsBestBeforeUpdateJob() {
         Set<Product> products = this.productRepository.findAll().stream()
                 .peek(p -> p.setBestBefore(generateRandomDate()))
                 .collect(Collectors.toSet());
 
         this.productRepository.saveAll(products);
+    }
+
+    @PostConstruct
+    @Transactional
+    public void restockProductsQuantity() {
+        this.productRepository.saveAll(this.productRepository.findAll().stream()
+                .peek(product -> product.setStockLevel(10))
+                .collect(Collectors.toSet())
+        );
     }
 
     private LocalDate generateRandomDate() {
