c16136f7821502c7d8840563616b64aac8017c45
Radoslav <radii2000@abv.bg>
2023-04-12 16:32:33 +0300

    Added currency sign on finalize

--- Angular/front-end/src/app/checkout/finalize/finalize.component.ts
+++ Angular/front-end/src/app/checkout/finalize/finalize.component.ts
@@ -35,7 +35,7 @@ export class FinalizeComponent implements OnInit {
     this.voucher$ = this.basketService.fetchVoucherIfPresent();
 
     this.discountedBasketTotal$ = this.store.select(selectDiscountedTotalState).pipe(
-      map(state => state ? (state.total <= 0 ? 'Free' : (state.total.toFixed(2) + ' lv.')) : undefined)
+      map(state => state ? (state.total <= 0 ? 'Free' : ('$' + state.total.toFixed(2) + ' lv.')) : undefined)
     );
   }
 
