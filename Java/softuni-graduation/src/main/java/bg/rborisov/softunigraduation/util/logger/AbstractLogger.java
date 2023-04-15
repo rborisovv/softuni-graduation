e94f74ec9dda942d306542d64816e0df871c19b7
Radoslav <radii2000@abv.bg>
2023-04-15 19:00:16 +0300

    Modified the AbstractLogger, so now if it cannot find a folder or a file, simply create it first and then log the message

--- Java/softuni-graduation/src/main/java/bg/rborisov/softunigraduation/util/logger/AbstractLogger.java
+++ Java/softuni-graduation/src/main/java/bg/rborisov/softunigraduation/util/logger/AbstractLogger.java
@@ -11,8 +11,22 @@ import java.time.format.DateTimeFormatter;
 public abstract class AbstractLogger {
     private static final String LOG_FOLDER = "C:\\Users\\Radoslav\\Documents\\Github\\softuni-graduation\\Java\\softuni-graduation\\src\\main\\resources\\logs\\";
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     protected void log(final String fileName, final String message, LoggerStatus loggerStatus) throws IOException {
+        File logsFolder = new File(LOG_FOLDER);
+
+        if (!logsFolder.exists()) {
+            // Create the logs folder if it doesn't exist
+            logsFolder.mkdir();
+        }
+
         File file = new File(LOG_FOLDER + fileName);
+
+        if (!file.exists()) {
+            // Create the file if it doesn't exist
+            file.createNewFile();
+        }
+
         PrintWriter printWriter = new PrintWriter(new java.io.FileWriter(file, true));
 
         String loggerBuilder = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")) +
